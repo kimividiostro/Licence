3003624db88b69dfe2ca50205c2e613d08b412bf
Milan <milanpizurica@icloud.com>
2021-06-23 00:26:57 +0200

    Refactored Licence model

--- src/models/licence.ts
+++ src/models/licence.ts
@@ -1,13 +1,13 @@
 import { LicenceType, LicenceStatus } from './licence-enums';
 
 export interface Licence {
-  // numbers are used as ids for simplicity
+  // Brojevi se koriste za id radi jednostavnosti
   id: number;
   type: LicenceType;
   numberOfCustomersAllowed?: number;
   numberOfCustomersSpent: number;
-  activationDate: Date;
-  expirationDate: Date;
+  activationDate?: Date;
+  expirationDate?: Date;
   totalAmount: number;
   status: LicenceStatus;
   businessID: number;
